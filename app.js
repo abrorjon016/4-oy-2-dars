5408646f5f19866f58c4c7eb104a546c2fde3e43
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-07-20 19:02:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,195 @@
+// 1-savol
+
+// let num = 4;
+
+// if (num > 0) {
+//   num ++;
+// }
+
+// console.log(num);
+
+// 2-savol
+
+// let num = -4;
+
+// if (num > 0) {
+//   num ++;
+// }else{
+//     num -= 2
+// }
+
+// console.log(num);
+
+// 3-savol
+
+// let num = 0;
+
+// if (num > 0) {
+//   num++;
+// } else if(num === 0){
+//     num += 10
+// }
+// else {
+//   num -= 2;
+// }
+
+// console.log(num);
+
+// 4-savol
+
+// let a = 4;
+// let b = -4;
+// let c = 4;
+
+// let sum = 0;
+
+// if (a > 0) {
+//     sum++
+// }
+// if (b > 0) {
+//     sum++
+// }
+// if (c > 0) {
+//     sum++
+// }
+
+// console.log(sum);
+
+// 5-savol
+
+// let a = 4;
+// let b = -4;
+// let c = 4;
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     musbat++
+// }else{
+//     manfiy++
+// }
+// if (b > 0) {
+//     musbat++
+// }else{
+//     manfiy++
+// }
+// if (c > 0) {
+//     musbat++
+// }else{
+//     manfiy++
+// }
+
+// console.log("musbat sonlarimiz  "+musbat);
+// console.log("manfiy sonlarimiz  "+manfiy);
+
+// 6-savol
+
+// let num1 = 5;
+// let num2 = 74;
+
+// if (num1 > num2) {
+//     console.log(num1);
+// }else{
+//     console.log(num2);
+// }
+
+// 7-savol
+
+// let num1 = 7;
+// let num2 = 2;
+
+// if (num1 < num2) {
+//     console.log(num1);
+// }else{
+//     console.log(num2);
+// }
+
+// 8-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let kata =0
+// let kichig =0
+// if (a>b) {
+//     kata=a
+// }else{
+//     kichig=a
+// }
+// if (a<b) {
+//     kata=b
+// }else{
+//     kichig=b
+// }
+// console.log(kata,'kata');
+// console.log(kichig,'kichig');
+
+// 9-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// if (a>b) {
+//    b+=a
+// }
+// console.log(a,'a');
+// console.log(b,'b');
+
+// 10-masal
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let res =0
+// if (a==b) {
+//     res+0
+// }
+// if (a!=b) {
+//     res=a+b
+// }
+
+// boolenlar
+// 1-masala
+// let a = 4;
+
+// if (a > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 4-masala
+// let a = 4;
+// let b = 5;
+
+// if (a > 2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// if (b < 3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 5-masala
+// let a = 4;
+// let b = 5;
+
+// if (a >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// if (b < -2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// // 6-masala
+// let num = 2;
+// let num2 = 3;
+// let num3 = 4;
+
+// if (a <= b <= c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
